86782be285fcc62dbf77ffd88eeb4852fc2a5d55
mnadeem <coolmind182006@gmail.com>
2015-02-27 23:45:40 +0530

    Added slf4j

--- src/main/java/com/prokarma/middleware/eeb/service/NotificationBrokerBean.java
+++ src/main/java/com/prokarma/middleware/eeb/service/NotificationBrokerBean.java
@@ -2,6 +2,8 @@ package com.prokarma.middleware.eeb.service;
 
 import javax.inject.Inject;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.switchyard.component.bean.Service;
 
 import com.prokarma.middleware.eeb.business.NotificationProcessor;
@@ -10,12 +12,15 @@ import com.prokarma.middleware.eeb.service.model.Notification;
 
 @Service(NotificationBroker.class)
 public class NotificationBrokerBean implements NotificationBroker {
+
+	private static Logger LOGGER = LoggerFactory.getLogger(NotificationBrokerBean.class);
+
 	@Inject
 	private NotificationProcessor processor;
 
 	@Override
-	public void notify(Notification notification) throws NotificationException {		
+	public void notify(Notification notification) throws NotificationException {
+		LOGGER.info("Receive notificaton {} ", notification);
 		processor.process(notification);
 	}
-
 }
